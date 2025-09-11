fd22cf3891b5ca1f13dc81e29d355b460a2eb960
Timothy Lin <55767165+Deodat-Lawson@users.noreply.github.com>
2025-09-10 20:42:54 -0400

    Update vite.config.ts

--- acm_website/vite.config.ts
+++ acm_website/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase or silence chunk size warnings
+    chunkSizeWarningLimit: 2000,
+  },
+  // Only show errors, not warnings
+  logLevel: 'error',
 })
